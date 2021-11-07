9608d596ba5205706f14b36b39d3ed4366ea6817
Antoine75-JS <hello@antoinegarcia.com>
2021-11-07 12:37:17 +0100

    setup redis session

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ require('dotenv').config();
 // Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
-
+const session = require('express-session');
+const redis = require('redis');
 const cors = require('cors');
+
 const routers = require('./routers');
-const { MONGO_IP, MONGO_PORT } = require('./config/config');
+const { MONGO_IP, MONGO_PORT, REDIS_URL, REDIS_PORT, SESSION_SECRET } = require('./config/config');
+
+// Redis store for auth
+let RedisStore = require('connect-redis')(session);
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+});
 
 // Creates express app
 const app = express();
@@ -28,6 +37,21 @@ const loopUntilConnected = () => {
 
 // Start connection loop
 loopUntilConnected();
+
+// Session middleware
+app.use(session({
+  store: new RedisStore({ client: redisClient }),
+  secret: SESSION_SECRET,
+  // TO REPARAM DEPENDING ON NEEDS
+  cookie: {
+    secure: false,
+    resave: false,
+    saveUninitialized: false,
+    httpOnly: true,
+    // maxAge: 7200000,
+    maxAge: 30000,
+  }
+}))
 
 // App init
 app.use(express.json());
